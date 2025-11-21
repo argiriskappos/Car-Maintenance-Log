9b26d089bf8bd92bbddfa6e9a8536923615321ab
argiriskappos <argiris.kappos@ontariotechu.net>
2025-11-21 16:14:05 -0500

    Final Commit 2

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,10 +1,42 @@
-const mongoose = require('mongoose');
+const MaintenanceRecord = require('../models/MaintenanceRecord');
 
-module.exports = function connectDB() {
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => {
-      console.error("MongoDB Error:", err);
-      process.exit(1);
-    });
+// LIST
+exports.index = async (req, res) => {
+  const records = await MaintenanceRecord.find().sort({ serviceDate: -1 });
+  res.render('records/list', { records });
+};
+
+// NEW FORM
+exports.newForm = (req, res) => {
+  res.render('records/new');
+};
+
+// CREATE
+exports.create = async (req, res) => {
+  await MaintenanceRecord.create(req.body.record);
+  res.redirect('/records');
+};
+
+// SHOW
+exports.show = async (req, res) => {
+  const record = await MaintenanceRecord.findById(req.params.id);
+  res.render('records/show', { record });
+};
+
+// EDIT FORM
+exports.editForm = async (req, res) => {
+  const record = await MaintenanceRecord.findById(req.params.id);
+  res.render('records/edit', { record });
+};
+
+// UPDATE
+exports.update = async (req, res) => {
+  await MaintenanceRecord.findByIdAndUpdate(req.params.id, req.body.record);
+  res.redirect(`/records/${req.params.id}`);
+};
+
+// DELETE
+exports.delete = async (req, res) => {
+  await MaintenanceRecord.findByIdAndDelete(req.params.id);
+  res.redirect('/records');
 };
