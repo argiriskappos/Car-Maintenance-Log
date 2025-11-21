5524419330874209a1c981e367ee6610da865d9e
argiriskappos <argiris.kappos@ontariotechu.net>
2025-11-21 16:43:36 -0500

    Final Commit 2

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-module.exports = function connectDB() {
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => {
-      console.error("MongoDB Error:", err);
-      process.exit(1);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Error:", error);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
