815411c66f9fab889ed89cd6fdf360a2835627da
argiriskappos <argiris.kappos@ontariotechu.net>
2025-11-22 17:09:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 // View engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
-const recordsRouter = require('./routes/records');
-app.use('/', recordsRouter);
-
-// 404 page
-app.use((req, res) => {
-  res.status(404).render('404', { url: req.originalUrl });
-});
-
-// Server start
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server running on port 3000")
-);
-
-
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
