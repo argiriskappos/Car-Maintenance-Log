7e1606a28f9b6ce7ca37a22dc29e2e61a4b45c58
argiriskappos <argiris.kappos@ontariotechu.net>
2025-11-29 14:09:28 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,11 @@
-// config/passport.js
-const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 const User = require('../models/User');
 
-module.exports = function(passportInstance) {
-  // Serialize user to session
-  passportInstance.serializeUser((user, done) => done(null, user.id));
-  passportInstance.deserializeUser(async (id, done) => {
+module.exports = function(passport) {
+  // Serialize & deserialize
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findById(id).lean();
       done(null, user);
@@ -17,7 +15,7 @@ module.exports = function(passportInstance) {
   });
 
   // Google Strategy
-  passportInstance.use(new GoogleStrategy({
+  passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
@@ -31,16 +29,18 @@ module.exports = function(passportInstance) {
           provider: 'google',
           providerId,
           displayName: profile.displayName,
-          email: profile.emails && profile.emails[0] && profile.emails[0].value,
-          avatar: profile.photos && profile.photos[0] && profile.photos[0].value
+          email: profile.emails?.[0]?.value,
+          avatar: profile.photos?.[0]?.value
         });
       }
       done(null, user);
-    } catch (err) { done(err); }
+    } catch (err) {
+      done(err);
+    }
   }));
 
   // GitHub Strategy
-  passportInstance.use(new GitHubStrategy({
+  passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.GITHUB_CALLBACK_URL,
@@ -49,8 +49,7 @@ module.exports = function(passportInstance) {
   async (accessToken, refreshToken, profile, done) => {
     try {
       const providerId = profile.id;
-      let email = null;
-      if (profile.emails && profile.emails.length) email = profile.emails[0].value;
+      let email = profile.emails?.[0]?.value || null;
       let user = await User.findOne({ provider: 'github', providerId });
       if (!user) {
         user = await User.create({
@@ -58,10 +57,12 @@ module.exports = function(passportInstance) {
           providerId,
           displayName: profile.displayName || profile.username,
           email,
-          avatar: profile.photos && profile.photos[0] && profile.photos[0].value
+          avatar: profile.photos?.[0]?.value
         });
       }
       done(null, user);
-    } catch (err) { done(err); }
+    } catch (err) {
+      done(err);
+    }
   }));
 };
