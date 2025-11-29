ce2cb76e5f14505ed5f47b1b1c7518ef8494ced0
argiriskappos <argiris.kappos@ontariotechu.net>
2025-11-29 13:39:26 -0500

    Update recordsController.js

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,41 +1,48 @@
 const MaintenanceRecord = require('../models/MaintenanceRecord');
 
-// LIST
+// LIST ALL RECORDS
 exports.index = async (req, res) => {
   const records = await MaintenanceRecord.find().sort({ serviceDate: -1 });
-  res.render('records/list', { records });
+  res.render('records/index', { records }); // you use index.ejs
 };
 
-// NEW FORM
+// SHOW NEW RECORD FORM
 exports.newForm = (req, res) => {
   res.render('records/new');
 };
 
-// CREATE
+// CREATE NEW RECORD
 exports.create = async (req, res) => {
-  await MaintenanceRecord.create(req.body.record);
-  res.redirect('/records');
+  try {
+    await MaintenanceRecord.create(req.body);  // USE req.body, not req.body.record
+    res.redirect('/records');
+  } catch (err) {
+    console.error(err);
+    res.render('records/new', { error: "Error creating record." });
+  }
 };
 
-// SHOW
+// SHOW SINGLE RECORD
 exports.show = async (req, res) => {
   const record = await MaintenanceRecord.findById(req.params.id);
+  if (!record) return res.redirect('/records');
   res.render('records/show', { record });
 };
 
-// EDIT FORM
+// SHOW EDIT FORM
 exports.editForm = async (req, res) => {
   const record = await MaintenanceRecord.findById(req.params.id);
+  if (!record) return res.redirect('/records');
   res.render('records/edit', { record });
 };
 
-// UPDATE
+// UPDATE RECORD
 exports.update = async (req, res) => {
-  await MaintenanceRecord.findByIdAndUpdate(req.params.id, req.body.record);
+  await MaintenanceRecord.findByIdAndUpdate(req.params.id, req.body);
   res.redirect(`/records/${req.params.id}`);
 };
 
-// DELETE
+// DELETE RECORD
 exports.delete = async (req, res) => {
   await MaintenanceRecord.findByIdAndDelete(req.params.id);
   res.redirect('/records');
